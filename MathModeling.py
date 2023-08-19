059878455747dbe58651cd921611f13c6ecb22ca
hL <1263164021@qq.com>
2023-08-19 16:01:58 +0800

    (19)增加 Topsis 方法

---
+++ MathModeling.py
@@ -0,0 +1,128 @@
+#数学建模常用方法类
+import numpy as np
+import math
+# @by haolian
+class MathModeling:
+    def __init__(self) -> None:
+        pass
+
+
+    #一致性检验
+    # 如果满足一致性检验则返回权重向量，否则返回None
+    @classmethod
+    def consistency_check(self, matrix):
+        # 检查矩阵的一致性
+        n = matrix.shape[0]
+        if n < 2:
+            print("矩阵太小，无法进行一致性检验！")
+            return
+        
+        # 计算矩阵的特征值和特征向量
+        eigenvalues, eigenvectors = np.linalg.eig(matrix)
+        max_eigenvalue = max(eigenvalues.real)
+        index = np.where(eigenvalues.real == max_eigenvalue)[0][0]
+        eigenvector = eigenvectors[:, index].real
+        normalized_eigenvector = eigenvector / np.sum(eigenvector)
+
+        # 计算一致性指标CI
+        lambda_max = max_eigenvalue
+        random_index = np.array([0, 0.58, 0.9, 1.12, 1.24, 1.32, 1.41, 1.45, 1.49, 1.51,1.54,1.56,1.58,1.59,1.5943])
+        consistency_index = (lambda_max - n) / (n - 1)
+        consistency_ratio = consistency_index / random_index[n - 1]
+        
+        # 判断一致性是否通过
+        if consistency_ratio < 0.1:
+            return normalized_eigenvector
+        else:
+            return None
+
+        ##################################一致性检验示例############################
+        # matrix = np.array([[1, 3, 5],
+        #                 [1/3, 1, 2],
+        #                 [1/5, 1/2, 1]])
+        #
+        # normalized_eigenvector=consistency_check(matrix)
+        ##########################################################################
+
+
+    # Topsis法
+    # 对指标进行对象的评分排名，将所有的指标转化为极大型指标
+    # 层次分析法的局限性：决策层不能过多，利用决策层指标已知数据
+    # impacts[i]有四种类型 max 、min 、interval 、mid
+    # 数值越接近1，表示该数据点越接近理想解；数值越接近0，表示该数据点越接近负理想解。
+    @classmethod
+    def topsis(self, data, weights, impacts):
+        # 正向化
+        normalized_data = np.zeros_like(data, dtype=float)
+        for i in range(data.shape[1]):
+            if impacts[i] == 'min':
+                normalized_data[:, i] = self.__topsis_min(data[:, i], np.max(data[:, i]))
+            elif impacts[i] == 'mid':
+                normalized_data[:, i] = self.__topsis_mid(data[:, i], np.min(data[:, i]), np.max(data[:, i]))
+            elif impacts[i] == 'interval':
+                normalized_data[:, i] = self.__topsis_interval(data[:, i], np.min(data[:, i]), np.max(data[:, i]), np.min(data[:, i]), np.max(data[:, i]))
+            elif impacts[i] == 'max':
+                normalized_data[:, i] = data[:, i]
+            else:
+                raise Exception("类型不正确！")
+
+        # 标准化
+        for i in range(data.shape[1]):
+            normalized_data[:, i] = normalized_data[:, i] / math.sqrt((sum(normalized_data[:, i] ** 2)))
+
+        # 计算综合评价指标
+        scores = np.zeros(data.shape[0], dtype=float)
+        for i in range(data.shape[0]):
+            scores[i] = sum(normalized_data[i, :] * weights)
+
+        return scores
+    
+    def __topsis_min(self,datas, x_max):
+        def normalization(data):
+            return x_max-data
+
+        return list(map(normalization, datas))
+
+
+    def __topsis_mid(self,datas, x_min, x_max):
+        def normalization(data):
+            if data <= x_min or data >= x_max:
+                return 0
+            elif data > x_min and data < (x_min + x_max) / 2:
+                return 2 * (data - x_min) / (x_max - x_min)
+            elif data < x_max and data >= (x_min + x_max) / 2:
+                return 2 * (x_max - data) / (x_max - x_min)
+
+        return list(map(normalization, datas))
+
+
+    def __topsis_interval(self,datas, x_min, x_max, x_minimum, x_maximum):
+        def normalization(data):
+            if data >= x_min and data <= x_max:
+                return 1
+            elif data <= x_minimum or data >= x_maximum:
+                return 0
+            elif data > x_max and data < x_maximum:
+                return 1 - (data - x_max) / (x_maximum - x_max)
+            elif data < x_min and data > x_minimum:
+                return 1 - (x_min - data) / (x_min - x_minimum)
+
+        return list(map(normalization, datas))
+    
+        ##################################一致性检验示例############################
+        # data = np.array([
+        #     [1000, 2000, 3000],
+        #     [150, 250, 350],
+        #     [20, 30, 40],
+        #     [2, 3, 4]
+        # ])
+
+        # weights = np.array([0.4, 0.3, 0.3])
+        # impacts = ["max"]*3
+        # res=MathModeling.topsis(data,weights,impacts)
+        # print(res)
+        ##########################################################################
+
+       
+
+
